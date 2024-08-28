bf7f8b2f8e6952badc47dfae945480240552ff73
rsaotome3150 <rsaotome@unc.edu>
2024-08-28 11:51:23 -0400

    finished exercise 00!

---
+++ Lessons/lesson3.py
@@ -0,0 +1,15 @@
+"""Practice with functions"""
+
+# from random import randint
+
+# print(randint(1, 100))
+
+
+# Function Definition
+def sum(num1: int, num2: int) -> int:
+    """return the sum of num1 and num2."""
+    return num1 + num2
+
+
+# Function Call
+print(sum(num1=2, num2=13))
